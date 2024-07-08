9e9e16de64273f57a28dbc372244cc94247c76c9
halitkalayci <halit.kalayci@ext.etiya.com>
2024-07-08 13:45:15 +0300

    init

---
+++ WebAPI/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    // localhost:8080/api/cart
+    [Route("api/[controller]")] // Attribute => 
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        // En az 1 alan farklı olmalı.
+
+
+        // Sistemden kaynak talep edildiğinde. GET -> Getirmek
+        [HttpGet]
+        public string SayHi()
+        {
+            return "Hi";
+        }
+
+        // Sisteme yeni kaynak eklenmesi talep edildiğinde. POST => POST
+        [HttpPost]
+        public string SayGoodbye()
+        {
+            return "Good Bye";
+        }
+
+        // PUT => Sepet eskiden şuydu şu anda bu
+        [HttpPut]
+        public string Update()
+        {
+            return "Http PUT";
+        }
+
+        // PATCH => 
+        [HttpPatch]
+        public string UpdateOnly()
+        {
+            return "Http Patch";
+        }
+        // DELETE => Kaynak silineceği zaman
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Http Delete";
+        }
+    }
+}
